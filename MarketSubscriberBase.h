45f53bafa26a3e3ec116b899470d35fca57c7ba4
thuyet <thuyet.pham@polaro.com>
2024-12-25 09:36:58 +0700

    update

--- MarketSubscriberBase.h
+++ MarketSubscriberBase.h
@@ -1,12 +1,23 @@
 #pragma once
+
+#include <type_traits>
+
+// Forward declaration of MarketSubscriberBase
+template <typename OrderBookType> class MarketSubscriberBase;
+
+// Concept to check if T is derived from MarketSubscriberBase<OrderBookType>
+template <typename T, typename OrderBookType>
+concept DerivedFromMarketSubscriberBase = std::is_base_of_v<MarketSubscriberBase<OrderBookType>, T>;
+
 template <typename OrderBookType>
 class MarketSubscriberBase {
 public:
-  template <class Derived>
-  auto onBookHandle(Derived* derivedObj, const OrderBookType& orderBook) {
-    /* ??? */
-    derivedObj->onBook(orderBook);
-  }
+    // CRTP-based delegation with concept
+    void onBookHandle(auto* derivedObj, const OrderBookType& orderBook)
+      requires DerivedFromMarketSubscriberBase<decltype(*derivedObj), OrderBookType> {
+        derivedObj->onBook(orderBook);
+    }
+
 private:
   auto onBook(const auto& orderBook); // defer implementation to derived classes
 };
